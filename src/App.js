2f371d7fb2a28c90147837a2f3f06c1f6f87e595
Priyanshu-bit <priyanshupatel56@gmail.com>
2023-04-10 15:38:02 +0530

    Minor Changes in Navbar

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import { Box } from '@mui/material';
 import './App.css';
 import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
-import Navbar from './components/Navbar';
+
 import Footer from './components/Footer';
 import Login from "./components/Login"
 
+
 const App = () => (
   <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
-    <Navbar />
+   
     <Login/>
+    
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/exercise/:id" element={<ExerciseDetail />} />
